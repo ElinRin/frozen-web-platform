e2e71d029422a09917be46f3f3e34488fdfb02f8
ElinRin <elin.rinnel@gmail.com>
2019-10-26 01:08:11 +0300

    feat: add main component App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Button } from "reactstrap";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <br />
+        <br />
+        <Button color="primary">Наташа работай</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
