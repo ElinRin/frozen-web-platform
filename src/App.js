acd0fd6945d64cfacb6724e41a1b0ff5df8e4924
bobrovskayaa <nataliya.bobrovskaya@phystech.edu>
2019-10-26 02:46:59 +0300

    feat: create login form component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Button } from "reactstrap";
+import LogInForm from './components/LogInForm/LogInForm.js'
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <br />
-        <br />
-        <Button color="primary">Наташа работай</Button>
+          <LogInForm />
       </header>
     </div>
   );
