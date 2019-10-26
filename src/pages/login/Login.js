9f6b6fef52415a71c655e3c14e8c505b7dd0ca7c
ElinRin <elin.rinnel@gmail.com>
2019-10-26 07:20:34 +0300

    feat: add Login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Button, Form, FormGroup, Input } from "reactstrap";
+import "./Login.css";
 export const Login = () => (
-  <Form>
-    <FormGroup>
-      <Input type="email" placeholder="Email" />
-    </FormGroup>
-    <FormGroup>
-      <Input type="password" placeholder="Password" />
-    </FormGroup>
-    <FormGroup>
+  <div className="login-container">
+    <Form className="login-form">
+      <FormGroup>
+        <Input type="email" placeholder="Email" />
+      </FormGroup>
+      <FormGroup>
+        <Input type="password" placeholder="Password" />
+      </FormGroup>
       <Button color="primary">Log in</Button>
-    </FormGroup>
-  </Form>
+      <Button color="link" size="sm">
+        Forgot password?
+      </Button>
+    </Form>
+  </div>
 );
