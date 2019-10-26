e2d359a985d3ef91e64a60ab3bf08344bbd1f5b3
bobrovskayaa <nataliya.bobrovskaya@phystech.edu>
2019-10-26 14:31:17 +0300

    feat: create modal window for request days off

--- src/components/square-layout/SquareLayout.js
+++ src/components/square-layout/SquareLayout.js
@@ -7,23 +7,30 @@ import {
 } from "reactstrap";
 
 import './SquareLayout.css'
+import { Modal } from "../modal/Modal";
 
 export const SquareLayout = () => (
   <Container >
     <Row>
       <Col xs="5" className='wrapper-sq-l'>
         <h5>Vacation</h5>
-        <Button color='primary'>25 дней</Button>
+        <Modal
+          buttonLabel="25 дней"
+        />
       </Col>
       <Col xs="5" className='wrapper-sq-l'>
         <h5>Sick leave</h5>
-        <Button color='primary'>6 дней</Button>
+        <Modal
+          buttonLabel="25 дней"
+        />
       </Col>
     </Row>
     <Row>
       <Col xs="5" className='wrapper-sq-l'>
         <h5>Remote work</h5>
-        <Button color='primary'>6 дней</Button>
+        <Modal
+          buttonLabel="25 дней"
+        />
       </Col>
     </Row>
   </Container>
