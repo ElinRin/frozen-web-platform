1ffe6962a3d2262e22969eeae6b49cab85f5ed42
chernega25 <chernega_nikita@mail.ru>
2019-10-27 08:33:08 +0100

    feat(main): profile picture and status

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {
   Col,
   Container,
@@ -11,36 +11,64 @@ import { NewsLayout } from "../../components/news-layout/NewsLayout";
 
 import photo from "../../mocks/photo_example.jpg";
 import "./Main.css";
+import {changeStatus, fetchMe} from "../../actions/user";
+import {ProfileContext} from "../../app/Context";
 
-export const Main = () => (
-  <Container className="wrapper-large">
-    <Row>
-      <Col xs="2" className="main-up-left-block main-left-block">
-        <Media src={photo} className="profile-photo-small"/>
-      </Col>
-      <Col xs="2" className="main-up-right-block main-left-block">
-        <h3>Everett Soto</h3>
-        <Button
-          color='primary'
-          className="profile-button"
-          onClick={() => {}} // TODO: менять статус при нажатии
-        >
-          On work
-        </Button>
-      </Col>
-      <Col xs="8">
-        <BirthdaysLayout />
-      </Col>
-    </Row>
-    <Row>
-      <Col xs="4" className="main-left-block main-down-block">
-        <div className="wrapper-days-off">
-          <DaysOffLayout />
-        </div>
-      </Col>
-      <Col xs="8">
-        <NewsLayout />
-      </Col>
-    </Row>
-  </Container>
-);
+export const Main = () => {
+
+  const [profile, profileDispatch] = useContext(ProfileContext);
+
+  useEffect(async () => {
+    await fetchMe(profileDispatch)
+  }, [profile.userId, ]);
+
+  const atHome = {pos: 0, label: 'At home', color: 'secondary'};
+  const onWork = {pos: 1, label: 'On work', color: 'primary'};
+  let initialStatus;
+  if (profile.status === 'On work') {
+    initialStatus = onWork;
+  } else {
+    initialStatus = atHome;
+  }
+  const [status, setStatus] = useState(initialStatus);
+
+  return (
+    <Container className="wrapper-large">
+      <Row>
+        <Col xs="2" className="main-up-left-block main-left-block">
+          <Media src={profile.profileImage} className="profile-photo-small"/>
+        </Col>
+        <Col xs="2" className="main-up-right-block main-left-block">
+          <h3>{profile.fullName}</h3>
+          <Button
+            color={status.color}
+            className="profile-button"
+            onClick={() => {
+              if (status.pos === 0) {
+                setStatus(onWork)
+              } else {
+                setStatus(atHome)
+              }
+              changeStatus(status.label, profileDispatch).catch(error => console.log(error))
+            }}
+          >
+            {status.label}
+          </Button>
+        </Col>
+        <Col xs="8">
+          <BirthdaysLayout/>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs="4" className="main-left-block main-down-block">
+          <div className="wrapper-days-off">
+            <DaysOffLayout/>
+          </div>
+        </Col>
+        <Col xs="8">
+          <NewsLayout/>
+        </Col>
+      </Row>
+    </Container>
+  )
+};
