62a7c738b73d95856953142ec67ea0b1b4ce6d1a
ElinRin <elin.rinnel@gmail.com>
2019-10-27 12:37:59 +0300

    fix: add useEffect

--- src/components/workplace-map_seat/WorkplaceMapSeat.js
+++ src/components/workplace-map_seat/WorkplaceMapSeat.js
@@ -1,38 +1,32 @@
-import React, {useState, useContext} from "react";
+import React, { useState, useContext, useEffect } from "react";
 import "./WorkplaceMapSeat.css";
 import {
   Button,
   Col,
-  Form,
-  FormGroup,
-  Input,
-  InputGroup,
-  InputGroupAddon,
-  Label,
-  ListGroup,
-  ListGroupItem,
   Media,
   Modal as ModalArea,
-  Row,
   ModalBody,
   ModalFooter,
   ModalHeader
 } from "reactstrap";
-import {UsersInfoContext, WorkPlaceInfoContext} from "../../app/Context";
-import {changeStatus, fetchUser} from "../../actions/user";
-import {navigate} from "hookrouter";
-import {reserveWorkPlace} from "../../actions/workPlace";
+import { UsersInfoContext, WorkPlaceInfoContext } from "../../app/Context";
+import { fetchUser } from "../../actions/user";
+import { navigate } from "hookrouter";
+import { reserveWorkPlace } from "../../actions/workPlace";
 
-
-export const WorkplaceMapSeat = ({id, x, y, color, uid}) => {
+export const WorkplaceMapSeat = ({ id, x, y, color, uid }) => {
   const [modal, setModal] = useState(false);
   const [modal2, setModal2] = useState(false);
   const [usersInfo, usersInfoDispatch] = useContext(UsersInfoContext);
-  const [workPlaceInfo, workPlaceInfoDispatch] = useContext(WorkPlaceInfoContext);
+  const [workPlaceInfo, workPlaceInfoDispatch] = useContext(
+    WorkPlaceInfoContext
+  );
 
-  if (uid) {
-    fetchUser(uid, usersInfoDispatch);
-  }
+  useEffect(() => {
+    if (uid) {
+      fetchUser(uid, usersInfoDispatch);
+    }
+  }, [uid, usersInfoDispatch]);
 
   const toggle = () => {
     if (uid) {
@@ -50,17 +44,30 @@ export const WorkplaceMapSeat = ({id, x, y, color, uid}) => {
         className="seat"
       ></div>
 
-      <ModalArea isOpen={modal}
-                 toggle={toggle}
-                 className="">
+      <ModalArea isOpen={modal} toggle={toggle} className="">
         <ModalHeader toggle={toggle}>This work place is not free</ModalHeader>
         <ModalBody onClick={() => navigate(`/u/${uid}`)}>
           <Col>
-            <Media src={(typeof usersInfo[uid] !== "undefined") ? usersInfo[uid].profileImage : ''} className="profile-photo-small" />
+            <Media
+              src={
+                typeof usersInfo[uid] !== "undefined"
+                  ? usersInfo[uid].profileImage
+                  : ""
+              }
+              className="profile-photo-small"
+            />
           </Col>
           <Col>
-            <h3>{(typeof usersInfo[uid] !== "undefined") ? usersInfo[uid].fullName : ''}</h3>
-            <h4>{(typeof usersInfo[uid] !== "undefined") ? usersInfo[uid].status : ''}</h4>
+            <h3>
+              {typeof usersInfo[uid] !== "undefined"
+                ? usersInfo[uid].fullName
+                : ""}
+            </h3>
+            <h4>
+              {typeof usersInfo[uid] !== "undefined"
+                ? usersInfo[uid].status
+                : ""}
+            </h4>
           </Col>
         </ModalBody>
         <ModalFooter>
@@ -70,23 +77,22 @@ export const WorkplaceMapSeat = ({id, x, y, color, uid}) => {
         </ModalFooter>
       </ModalArea>
 
-      <ModalArea isOpen={modal2}
-                 toggle={toggle}
-                 className="">
+      <ModalArea isOpen={modal2} toggle={toggle} className="">
         <ModalHeader toggle={toggle}>This work place is free</ModalHeader>
         <ModalBody>
-          <h2>
-            You can sit here.
-          </h2>
+          <h2>You can sit here.</h2>
         </ModalBody>
         <ModalFooter>
           <Button color="secondary" onClick={toggle}>
             Cancel
           </Button>
-          <Button color="primary" onClick={() => {
-            reserveWorkPlace(id, workPlaceInfoDispatch);
-            toggle();
-          }}>
+          <Button
+            color="primary"
+            onClick={() => {
+              reserveWorkPlace(id, workPlaceInfoDispatch);
+              setModal2(!modal2);
+            }}
+          >
             Sit here
           </Button>
         </ModalFooter>
@@ -94,5 +100,3 @@ export const WorkplaceMapSeat = ({id, x, y, color, uid}) => {
     </>
   );
 };
-
-
