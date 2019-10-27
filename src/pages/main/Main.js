52f35d49b2d23f98b7f47a7c3edd8e3cd5af1ce8
ElinRin <elin.rinnel@gmail.com>
2019-10-27 10:49:44 +0300

    fix: fix useEffect

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,31 +1,28 @@
-import React, {useContext, useEffect, useState} from "react";
-import {
-  Col,
-  Container,
-  Row,
-  Media, Button
-} from "reactstrap";
+import React, { useContext, useEffect, useState } from "react";
+import { Col, Container, Row, Media, Button } from "reactstrap";
 import { DaysOffLayout } from "../../components/daysoff-layout/DaysOffLayout";
 import { BirthdaysLayout } from "../../components/birthdays-layout/BirthdaysLayout";
 import { NewsLayout } from "../../components/news-layout/NewsLayout";
 
 import photo from "../../mocks/photo_example.jpg";
 import "./Main.css";
-import {changeStatus, fetchMe} from "../../actions/user";
-import {ProfileContext} from "../../app/Context";
+import { changeStatus, fetchMe } from "../../actions/user";
+import { ProfileContext } from "../../app/Context";
 
 export const Main = () => {
-
   const [profile, profileDispatch] = useContext(ProfileContext);
 
-  useEffect(async () => {
-    await fetchMe(profileDispatch)
-  }, [profile.userId, ]);
+  useEffect(() => {
+    async function fetchData() {
+      await fetchMe(profileDispatch);
+    }
+    fetchData();
+  }, [profile.userId, profileDispatch]);
 
-  const atHome = {pos: 0, label: 'At home', color: 'secondary'};
-  const onWork = {pos: 1, label: 'On work', color: 'primary'};
+  const atHome = { pos: 0, label: "At home", color: "secondary" };
+  const onWork = { pos: 1, label: "On work", color: "primary" };
   let initialStatus;
-  if (profile.status === 'On work') {
+  if (profile.status === "On work") {
     initialStatus = onWork;
   } else {
     initialStatus = atHome;
@@ -36,7 +33,7 @@ export const Main = () => {
     <Container className="wrapper-large">
       <Row>
         <Col xs="2" className="main-up-left-block main-left-block">
-          <Media src={profile.profileImage} className="profile-photo-small"/>
+          <Media src={profile.profileImage} className="profile-photo-small" />
         </Col>
         <Col xs="2" className="main-up-right-block main-left-block">
           <h3>{profile.fullName}</h3>
@@ -45,30 +42,32 @@ export const Main = () => {
             className="profile-button"
             onClick={() => {
               if (status.pos === 0) {
-                setStatus(onWork)
+                setStatus(onWork);
               } else {
-                setStatus(atHome)
+                setStatus(atHome);
               }
-              changeStatus(status.label, profileDispatch).catch(error => console.log(error))
+              changeStatus(status.label, profileDispatch).catch(error =>
+                console.log(error)
+              );
             }}
           >
             {status.label}
           </Button>
         </Col>
         <Col xs="8">
-          <BirthdaysLayout/>
+          <BirthdaysLayout />
         </Col>
       </Row>
       <Row>
         <Col xs="4" className="main-left-block main-down-block">
           <div className="wrapper-days-off">
-            <DaysOffLayout/>
+            <DaysOffLayout />
           </div>
         </Col>
         <Col xs="8">
-          <NewsLayout/>
+          <NewsLayout />
         </Col>
       </Row>
     </Container>
-  )
+  );
 };
