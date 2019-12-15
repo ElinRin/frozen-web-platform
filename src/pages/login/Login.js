2f08d9ba487c84a184798bcb37dc8b8450cda466
Elina Denisova <elin.rinnel@gmail.com>
2019-12-15 10:49:23 +0300

    fix: login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -4,26 +4,46 @@ import { navigate } from "hookrouter";
 import { Button, Form, FormGroup, Input } from "reactstrap";
 import { firebaseTools } from "../../utils/firebase";
 import { ProfileContext } from "../../app/Context";
-import { loginUser } from "../../actions/user";
+import { loginUser, loginUserByToken } from "../../actions/user";
 import "./Login.css";
 
 export const Login = ({ history }) => {
-  const [cookies, setCookie, removeCookie] = useCookies(["token"]);
+  const [cookies, setCookie] = useCookies(["token"]);
   const [profile, profileDispatch] = useContext(ProfileContext);
+  const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  if (!(profile.userId && profile.userId.length)) {
+    if (cookies.token) {
+      loginUserByToken(cookies.token, profileDispatch);
+      navigate("./main", true);
+    }
+  }
+
   const handleSubmit = async event => {
     event.preventDefault();
-    await loginUser({ email, password }, profileDispatch);
+    if (
+      email &&
+      email.trim().length > 0 &&
+      password &&
+      password.trim().length > 0
+    ) {
+      await loginUser({ email, password }, profileDispatch);
 
-    firebaseTools
-      .currentUser()
-      .getIdToken()
-      .then(token => {
-        setCookie("token", token, {
-          path: "/"
-        });
-      });
+      if (!firebaseTools.currentUser()) {
+        setError(true);
+      } else {
+        firebaseTools
+          .currentUser()
+          .getIdToken()
+          .then(token => {
+            setCookie("token", token, {
+              path: "/"
+            });
+          });
+      }
+    }
   };
 
   const testLogin = async () => {
@@ -53,32 +73,42 @@ export const Login = ({ history }) => {
 
   return (
     <div className="login-container">
-      <Form className="login-form" onSubmit={handleSubmit}>
-        <FormGroup>
-          <Input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={({ currentTarget: { value } }) => setEmail(value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={({ currentTarget: { value } }) => setPassword(value)}
-          />
-        </FormGroup>
-        <Button color="primary">Log in</Button>
-        <Button color="link" size="sm">
-          Forgot password?
-        </Button>
-      </Form>
+      <div className="login-form-container">
+        <Form className="login-form" onSubmit={handleSubmit}>
+          <FormGroup>
+            <Input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={({ currentTarget: { value } }) => {
+                setError(false);
+                setEmail(value);
+              }}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={({ currentTarget: { value } }) => {
+                setError(false);
+                setPassword(value);
+              }}
+            />
+          </FormGroup>
+          <Button color="primary">Log in</Button>
+        </Form>
 
-      <Button onClick={testLogin} color="primary">
-        Guest log in
-      </Button>
+        <div className="error">
+          {error && (
+            <span>
+              Неверный логин или пароль. Если Вы не помните свои данные для
+              входа, обратитесь к вашему hr.
+            </span>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
